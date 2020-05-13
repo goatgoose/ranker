7d4ae75ef979367f7f844577c084edced473233b
joshuachilds <jhchilds@uvm.edu>
2020-05-13 06:07:18 -0400

    subclass node

--- graph.py
+++ graph.py
@@ -12,6 +12,11 @@ class Node:
     def __repr__(self):
         return str(self)
 
+class ImagesNode(Node):
+    def __init__(self, id_, images):
+        super().__init__(id_)
+
+        self.images = images
 
 class Edge:
     def __init__(self, tail: Node, head: Node):
