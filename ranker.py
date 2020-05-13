cdb8956c71c1cbac3fa9f34b7055fed9f7833d1b
joshuachilds <jhchilds@uvm.edu>
2020-05-13 07:28:59 -0400

    implemented ranking with images nodes

--- ranker.py
+++ ranker.py
@@ -4,21 +4,28 @@ from graph import Edge, Graph, Node
 class Ranker(Graph):
     def __init__(self, collection):
         super().__init__()
-        
+
         self.collection = collection
         for image in collection:
-            images_node = Node(frozenset(image))
+            images_node = ImagesNode([image])
             self.add_node(images_node)
 
     def add_ranking(self, less, greater, equal=False):
-        less_node = self.nodes[frozenset(less)]
-        greater_node = self.nodes[frozenset(greater)]
+        less_node = self.nodes[frozenset([less])]
+        greater_node = self.nodes[frozenset([greater])]
 
         parent = None
         if equal:
-            parent = Node(less_node.id.union(greater_node.id))
+            parent = ImagesNode(less_node.id.union(greater_node.id))
         else:
-            parent = Node(greater_node.id.copy())
+            parent = ImagesNode(greater_node.id.copy())
+        parent.depth = greater_node.depth
 
         self.add_edge(Edge(parent, less_node))
         self.add_edge(Edge(parent, greater_node))
+
+class ImagesNode(Node):
+    def __init__(self, images):
+        super().__init__(frozenset(images))
+
+        self.depth = 0
