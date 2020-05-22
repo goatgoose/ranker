63560287c506d5dc6988db8cbe203e16fc493372
joshuachilds <jhchilds@uvm.edu>
2020-05-22 12:51:40 -0400

    no longer needed

--- ranker_old.py
+++ ranker_old.py
@@ -7,7 +7,7 @@ class Ranker(Graph):
 
         self.collection = collection
         for image in collection:
-            images_node = ImagesNode([image])
+            images_node = RankableNode([image])
             self.add_node(images_node)
 
     def add_ranking(self, less, greater, equal=False):
@@ -15,12 +15,12 @@ class Ranker(Graph):
         greater_node = self.nodes[frozenset([greater])]
 
         if equal:
-            tie_group = ImagesNode(less_node.id.union(greater_node.id))
+            tie_group = RankableNode(less_node.id.union(greater_node.id))
             self.add_node(tie_group)
             self.remove_node(less_node)
             self.remove_node(greater_node)
         else:
-            parent = ImagesNode(greater_node.id.copy())
+            parent = RankableNode(greater_node.id.copy())
             parent.depth = greater_node.depth
             self.add_edge(Edge(parent, less_node))
             self.add_edge(Edge(parent, greater_node))
@@ -31,7 +31,7 @@ class Ranker(Graph):
         not_considered = set()
 
         for node in self.nodes:
-            if node not in considered:
+            if node in not_considered:
                 continue
             elif len(node.edges) > 0:
                 not_considered.add(node)
@@ -40,14 +40,14 @@ class Ranker(Graph):
                 considered.add(node)
 
         sorted_considered = sorted(considered, key=lambda node: node.depth)
-        return sorted_considered[0], sorted_considered[1]
+        return sorted_considered[0]
 
     @property
     def rank(self):
         return {images: node.depth for images, node in self.nodes.items()}
 
 
-class ImagesNode(Node):
+class RankableNode(Node):
     def __init__(self, images):
         super().__init__(frozenset(images))
 
