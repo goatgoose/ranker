72a74f5d485f238e7b38ab80dc56d4149567c93f
joshuachilds <jhchilds@uvm.edu>
2020-05-23 10:04:26 -0400

    database setup and connection class

--- database.py
+++ database.py
@@ -1,10 +1,58 @@
 import mysql.connector
 import json
 
-with open("config.json") as json_data_file:
-    configs = json.load(json_data_file)
 
-cnx = mysql.connector.connect(user=configs['mysql']['user'], password=configs['mysql']['passwd'],
-                              host=configs['mysql']['host'],
-                              database=configs['mysql']['db'])
-cnx.close()
+class Database:
+    def __init__(self, db_name):
+        configs = self._get_configs()
+        self._cnx = mysql.connector.connect(
+            user=configs['mysql']['user'],
+            password=configs['mysql']['passwd'],
+            host=configs['mysql']['host'],
+            database=db_name
+        )
+        self._cursor = self._cnx.cursor()
+
+    @property
+    def connection(self):
+        return self._cnx
+
+    @property
+    def cursor(self):
+        return self._cnx.cursor()
+
+    def _get_configs(self):
+        with open("config.json") as data:
+            configs = json.load(data)
+        return configs
+
+    def close(self, commit=True):
+        if commit:
+            self._cnx.commit()
+        self._cnx.close()
+
+    def execute(self, sql, params=None):
+        self._cursor.execute(sql, params or ())
+
+
+if __name__ == '__main__':
+    create = '''CREATE DATABASE ranker_db;'''
+
+    db = Database('ranker_db')
+
+    create_table = '''
+        CREATE TABLE `rankable_items` (
+        `id` binary(32) NOT NULL DEFAULT '\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0',
+        `filename` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+        `caption` varchar(100) DEFAULT NULL,
+        PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    '''
+
+    sql = '''   
+        INSERT INTO rankable_items(id, filename, caption)
+        VALUES(UUID_TO_BIN(UUID()), %s, %s)
+    '''
+    values = ('test.png', 'hello there caption')
+    db.execute(sql, values)
+    db.close()
