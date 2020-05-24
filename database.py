e919bc5914dc7205a575dc6c5540d6dc6cf99d05
joshuachilds <jhchilds@uvm.edu>
2020-05-23 21:51:10 -0400

    init database created, config required

--- database.py
+++ database.py
@@ -1,15 +1,15 @@
-import mysql.connector
+import mysql.connector #https://dev.mysql.com/doc/connector-python/en/
 import json
 
 
 class Database:
-    def __init__(self, db_name):
+    def __init__(self, db_name=None):
         configs = self._get_configs()
         self._cnx = mysql.connector.connect(
             user=configs['mysql']['user'],
             password=configs['mysql']['passwd'],
             host=configs['mysql']['host'],
-            database=db_name
+            database=db_name or ()
         )
         self._cursor = self._cnx.cursor()
 
@@ -32,27 +32,32 @@ class Database:
         self._cnx.close()
 
     def execute(self, sql, params=None):
-        self._cursor.execute(sql, params or ())
+        self._cursor.execute(sql, params or (), multi=True)
 
 
 if __name__ == '__main__':
-    create = '''CREATE DATABASE ranker_db;'''
-
-    db = Database('ranker_db')
+    create_db = '''CREATE DATABASE ranker_db;'''
+    db = Database()
+    db.execute(create_db)
 
     create_table = '''
-        CREATE TABLE `rankable_items` (
-        `id` binary(32) NOT NULL DEFAULT '\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0\\0',
-        `filename` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
-        `caption` varchar(100) DEFAULT NULL,
-        PRIMARY KEY (`id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-    '''
+            USE ranker_db;
+            CREATE TABLE `rankable_items` (
+            `id` binary(32) NOT NULL DEFAULT '\\0\\0\\0\\0\\0\\0\\0\\0',
+            `filename` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+            `caption` varchar(100) DEFAULT NULL,
+            PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+        '''
+    db.execute(create_table)
 
-    sql = '''   
-        INSERT INTO rankable_items(id, filename, caption)
-        VALUES(UUID_TO_BIN(UUID()), %s, %s)
-    '''
-    values = ('test.png', 'hello there caption')
-    db.execute(sql, values)
     db.close()
+
+
+    # sql = '''
+    #     INSERT INTO rankable_items(id, filename, caption)
+    #     VALUES(UUID_TO_BIN(UUID()), %s, %s)
+    # '''
+    # values = ('test.png', 'hello there caption')
+    # db.execute(sql, values)
+    # db.close()
